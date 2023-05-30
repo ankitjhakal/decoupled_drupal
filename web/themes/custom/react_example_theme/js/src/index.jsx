1241a496c0eb5180cb5240eca3b96d390623d5e7
Ankit Jhakal <ankitjhakal@Ankits-MacBook-Pro.local>
2023-05-30 15:21:26 +0530

    Decoupled-site: Created a component to fetch usage data from third-party.

--- web/themes/custom/react_example_theme/js/src/index.jsx
+++ web/themes/custom/react_example_theme/js/src/index.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+/* Import Components */
+import DrupalProjectStats from './components/DrupalProjectStats';
 
+const Main = () => (
+  <DrupalProjectStats projectName="drupal" />
+);
+
+ReactDOM.render(<Main/>, document.getElementById('react-app-2'));
 // Example 1: Simple "Hello, World" code
 ReactDOM.render(
   <h1>Hello there - world!</h1>,
